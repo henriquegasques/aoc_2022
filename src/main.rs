71b25ae0d10dbdf800833b9ff753a8807887b021
Henrique Gasques <henriquegasques@gmail.com>
2022-12-02 12:08:29 -0300

    First day - Solutions for both puzzles

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use day_one::day_one::{first_puzzle, second_puzzle};
+
+mod day_one;
+
+fn main() {
+    println!("Day one first puzzle answer: {}", first_puzzle::solution());
+    println!("Day one second puzzle answer: {}", second_puzzle::solution());
+}
