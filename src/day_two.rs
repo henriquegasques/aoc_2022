fb8ca5794efc2a9420bb1183aff47480e16bae9e
Henrique Gasques <henriquegasques@gmail.com>
2022-12-05 22:48:29 -0300

    Second day - Solution for second puzzle

--- src/day_two.rs
+++ src/day_two.rs
@@ -27,6 +27,25 @@ pub mod first_puzzle {
 
 pub mod second_puzzle {
     pub fn solution() -> i32 {
-        unimplemented!()
+        let input = std::fs::read_to_string("./puzzle_inputs/day_two.txt").expect("Cannot read input file.");
+        let mut total_score = 0;
+        let mut strategies = input.lines();
+
+        while let Some(strategy) = strategies.next() {
+            match strategy.split_once(' ') {
+                Some(("A", "X")) => total_score += 3,
+                Some(("A", "Y")) => total_score += 4,
+                Some(("A", "Z")) => total_score += 8,
+                Some(("B", "X")) => total_score += 1,
+                Some(("B", "Y")) => total_score += 5,
+                Some(("B", "Z")) => total_score += 9,
+                Some(("C", "X")) => total_score += 2,
+                Some(("C", "Y")) => total_score += 6,
+                Some(("C", "Z")) => total_score += 7,
+                _ => unreachable!()
+            }
+        }
+
+        total_score
     }
 }
