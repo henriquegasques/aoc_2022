63e1ccc4c67c09ae2f9236544be322dfd7bf393d
Henrique Gasques <henriquegasques@gmail.com>
2022-12-06 01:02:30 -0300

    Refactor Third Day's First Puzzle

--- src/day_three.rs
+++ src/day_three.rs
@@ -2,15 +2,10 @@ pub mod first_puzzle {
     use std::collections::HashSet;
 
     pub fn solution() -> usize {
-        const LOWERCASE_CODE_OFFSET: usize = 96;
-        const UPPERCASE_CODE_OFFSET: usize = 38;
-
         let input = std::fs::read_to_string("./puzzle_inputs/day_three.txt").expect("Cannot read input file.");
+        let rucksacks = input.lines();
 
-        let mut rucksacks = input.lines();
-        let mut priority_sum = 0;
-
-        while let Some(rucksack) = rucksacks.next() {
+        rucksacks.fold(0, |sum, rucksack| {
             let (first_compartment, second_compartment) = rucksack.split_at(rucksack.len() / 2);
 
             let first_set: HashSet<char> = first_compartment.chars().collect();
@@ -18,19 +13,24 @@ pub mod first_puzzle {
 
             let shared_item = first_set.intersection(&second_set).next().unwrap();
 
-            if shared_item.is_lowercase() {
-                priority_sum += *shared_item as usize - LOWERCASE_CODE_OFFSET;
-            } else {
-                priority_sum += *shared_item as usize - UPPERCASE_CODE_OFFSET;
-            }
-        }
-
-        priority_sum
+            sum + super::priority_of(shared_item)
+        })
     }
 }
 
 pub mod second_puzzle {
     pub fn solution() -> i32 {
         unimplemented!()
+    }
+}
+
+fn priority_of(item: &char) -> usize {
+    const LOWERCASE_CODE_OFFSET: usize = 96;
+    const UPPERCASE_CODE_OFFSET: usize = 38;
+
+    if item.is_lowercase() {
+        *item as usize - LOWERCASE_CODE_OFFSET
+    } else {
+        *item as usize - UPPERCASE_CODE_OFFSET
     }
 }
